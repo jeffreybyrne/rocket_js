6b09682520c2e998a380bcbf468a298af4450532
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-03 15:30:24 -0400

    fixed the first test

--- rocket.test.js
+++ rocket.test.js
@@ -5,13 +5,19 @@ describe('Rocket', () => {
 
   describe('constructor', () => {
     test('it should set default attributes if nothing is passed', () => {
-      newRocket = Rocket.constructor();
+      let newRocket = new Rocket();
 
-      expect(newRocket).toBeDefined();
+      expect(newRocket).toBeInstanceOf(Rocket);
+      expect(typeof newRocket.name).toBe("string");
+      expect(typeof newRocket.colour).toBe("string");
     });
 
-    test("it should set the rocket's name if provided", () => {
-      // ...
+    test.skip("it should set the rocket's name if provided", () => {
+      let newRocket = new Rocket({name: "ICBM"});
+
+      result = newRocket.name;
+
+      expect(result).toEqual("ICBM");
     });
   });
 
