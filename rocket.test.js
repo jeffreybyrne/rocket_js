51e2a679547ecb609ecd56aeff9f37bd7271de5b
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-03 14:59:41 -0400

    got one test working

--- rocket.test.js
+++ rocket.test.js
@@ -5,7 +5,9 @@ describe('Rocket', () => {
 
   describe('constructor', () => {
     test('it should set default attributes if nothing is passed', () => {
-      // ...
+      newRocket = Rocket.constructor();
+
+      expect(newRocket).toBeDefined();
     });
 
     test("it should set the rocket's name if provided", () => {
